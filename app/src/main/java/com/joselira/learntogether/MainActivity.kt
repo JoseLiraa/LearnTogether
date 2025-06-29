8ba4169e52facf7ccffe6f50eeecb6e7f285a713
Jose <josmliraa@gmail.com>
2025-06-29 01:42:35 -0600

    Adding Composable Image and Text

--- app/src/main/java/com/joselira/learntogether/MainActivity.kt
+++ app/src/main/java/com/joselira/learntogether/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
@@ -12,6 +14,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -41,12 +44,29 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun LearnText(title: String, first: String, second: String, modifier: Modifier = Modifier) {
-    Text(
-        text = title,
+    Column(
         modifier = modifier
     )
-}
+    {
+        Image(
+            painterResource(id = R.drawable.bg_compose_background),
+            contentDescription = stringResource(R.string.content_description),
 
+            )
+        Text(
+            text = title,
+            modifier = modifier
+        )
+        Text(
+            text = first,
+            modifier = modifier
+        )
+        Text(
+            text = second,
+            modifier = modifier
+        )
+    }
+}
 
 /*@Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
@@ -55,7 +75,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         modifier = modifier
     )
 }*/
-//
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
